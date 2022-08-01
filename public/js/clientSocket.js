80badb92a54cadcd115808010ee672cbd3457675
Ankesh01 <70638498+Ankesh01@users.noreply.github.com>
2022-08-01 22:15:56 +0530

    Update clientSocket.js

--- public/js/clientSocket.js
+++ public/js/clientSocket.js
@@ -1,6 +1,6 @@
 var connected = false;
 
-var socket = io("http://localhost:3003")
+var socket = io("https://social-media-web0.herokuapp.com")
 socket.emit("setup", userLoggedIn);
 
 socket.on("connected", () => connected = true);
